31157befbe161a62017af8f35f9986a21ccc8d1f
Harsha Naidu <harsha4.naidu@gmail.com>
2021-02-23 19:18:00 -0800

    added code for new post and modified home page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+    before_action :find_post, only:[:show, :edit, :update, :destroy]
+
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post=Post.new post_params
+       
+        if @post.save
+           
+            redirect_to post_path(@post.id) # show page   
+        else
+            render :new
+        end
+    end
+
+    def index
+        @posts = Post.all.order(created_at: :desc)
+    end
+
+    def show
+       
+    end
+
+    def edit
+
+    end
+
+    def destroy   
+        @post.destroy
+        redirect_to posts_path
+    end
+
+    def update
+        if @post.update post_params
+           
+            redirect_to post_path(@post.id)
+        else
+            render :edit
+        end
+    end
+
+
+    private
+    def find_post
+        @post=Post.find params[:id]
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :description)
+    end
+    
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get("/",{to:"welcome#home", as: :root})
 
   resource :welcome, only:[:home]
+  resources :posts
 end
